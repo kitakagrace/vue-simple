82ccea512823b589dd3c0defb2d630ced2f1e109
prophetgrace <kitakagrace@gmail.com>
2022-03-09 14:11:19 +0300

    First Commit

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <v-img
+  lazy-src="https://picsum.photos/id/11/10/6"
+  max-height="146"
+  max-width=""
+  src="https://picsum.photos/id/11/500/300"
+></v-img>
+<h1>About Us</h1>
+  </v-container>
+</template>
---
+++ src/views/Register.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container>
+        <v-app>
+            <v-card  width="500px" class = "my-auto mx-auto">
+                <v-card-title>
+                    REGISTER
+                </v-card-title>
+                <v-card-text>
+                    <v-form>
+
+                        <v-text-field
+                            label = "Email"
+                            prepend-icon = "mdi-account-circle"
+                            required />
+                        <v-text-field
+                            label = "Password"
+                            required
+                            prepend-icon = "mdi-lock"
+                        />
+                         <v-checkbox
+                            class = "my-4"
+                            v-model="ex4"
+                            label="Agree to terms and conditions"
+                            color="warning"
+                            value=""
+                            hide-details
+                            ></v-checkbox>
+                        <v-divider></v-divider>
+                        <v-btn
+                        color = "warning"
+                        class = "my-4"
+                        >
+                            REGISTER
+                        </v-btn>
+                    </v-form>
+                </v-card-text>
+            </v-card>
+        </v-app>
+    </v-container>
+</template>
